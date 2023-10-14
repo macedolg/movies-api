557007e20868943c5dd80c6faca73f68620ddc5e
Lucas Gabriel Macêdo <lucasgabriel060405@gmail.com>
2023-10-14 01:26:01 -0300

    [Add] Criação do Serviço Review

---
+++ src/main/java/dev/macedolg/movies/services/ReviewService.java
@@ -0,0 +1,30 @@
+package dev.macedolg.movies.services;
+
+import dev.macedolg.movies.model.Movie;
+import dev.macedolg.movies.model.Review;
+import dev.macedolg.movies.repository.iReviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+
+    @Autowired
+    private iReviewRepository reviewRepo;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+    public Review createReview(String reviewBody, String imdbId) {
+        Review review = reviewRepo.insert(new Review(reviewBody));
+
+        mongoTemplate.update(Movie.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviewIds").value(review))
+                .first();
+
+        return review;
+    }
+}
